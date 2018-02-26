2a02201b994dfde8ce94066438e50a232eb74594
Andrei <mailstorme@mail.ru>
2018-02-26 11:34:12 +0300

    myCommit

---
+++ TriangleArea.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassTestDel
+{
+    public class TriangleArea
+    {
+        static public double Area(double a, double b, double c)
+        {
+            if(a <= 0 || b <= 0 || c <= 0 )
+                throw (new Exception("Error: Invalid side length!"));
+
+            double x = 0, y = 0, z = 0;
+
+            if (a > b && a > c)
+            {
+                z = a;
+                x = b;
+                y = c;
+            }
+            else if (b > a && b > c)
+            {
+                z = b;
+                x = a;
+                y = c;
+            }
+            else if (c > a && c > b)
+            {
+                z = c;
+                x = b;
+                y = a;
+            }
+            else
+            {
+                throw (new Exception("Error: Hypotenuse not found!"));
+            }
+
+            if (z * z != a * a + b * b)
+            {
+                if (z > x + y)
+                    throw (new Exception("Error: Incorrect triangle!"));
+                else
+                    throw (new Exception("Error: Triangle is not rectangular!"));
+            }
+
+            return x * y * 1/2;
+        }
+    }
+}
---
+++ TriangleAreaTest.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ClassTestDel.Tests
+{
+    [TestClass]
+    public class TriangleAreaTest
+    {
+        [TestMethod]
+        public void Area_3_4_5()
+        {
+            double a = 3, b = 4, c = 5;
+            double expected = 6;
+
+            double actual = TriangleArea.Area(a, b, c);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod]
+        public void Area_m1_4_5()
+        {
+            double a = -1, b = 4, c = 5;
+            double expected = 6;
+
+            double actual = TriangleArea.Area(a, b, c);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod]
+        public void Area_2_4_5()
+        {
+            double a = 2, b = 4, c = 5;
+            double expected = 6;
+
+            double actual = TriangleArea.Area(a, b, c);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod]
+        public void Area_3_3_3()
+        {
+            double a = 3, b = 3, c = 3;
+            double expected = 6;
+
+            double actual = TriangleArea.Area(a, b, c);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod]
+        public void Area_2_4_8()
+        {
+            double a = 2, b = 4, c = 8;
+            double expected = 4;
+
+            double actual = TriangleArea.Area(a, b, c);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
